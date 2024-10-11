0f837938001b0ea51a99edae01fa9968397b6419
c4llery <anton.rusanov.04@gmail.com>
2024-10-11 14:52:20 +0300

    update model in API and add stub function

--- sandbox/main.py
+++ sandbox/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 import pandas as pd
-from datetime import datetime
+from datetime import date
+import algo
+from pydantic import BaseModel
+
+class Data(BaseModel):
+    start: date
+    finish: date
+    result: str
+
+
 
 data = pd.read_csv("data.csv")
 data["time"] = pd.to_datetime(data["ts"], format='mixed')
@@ -17,3 +26,8 @@ def find_timestamp(timestamps: str):
     finish = times.at[1, "time"]
     piece_of_data = data[(finish >= data["time"]) & (data['time'] >= start)]
     return piece_of_data.to_csv()
+
+@app.post("/")
+def getData(data: Data):
+    data.result = algo.work()
+    return data
