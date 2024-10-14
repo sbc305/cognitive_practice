0e11618b76069b307c471e2f19908a57605042c0
VariPor <maiorova.ev@phystech.edu>
2024-10-14 17:27:28 +0300

    add averaging

--- core/alg_modes.py
+++ core/alg_modes.py
@@ -10,18 +10,17 @@ def EMD(cte: pd.Series):
     return imf
 
 
-def calculate_square_sum(x: np.array):
-    return sum([i * i for i in x])
+def calculate_mean_square_sum(x: np.array):
+    return sum([i * i for i in x]) / len(x)
 
 
 def compare_modes(empirical_modes: pd.Series):
-    etalon = calculate_square_sum(etalon_data.get_etalon_emd())
-    current = calculate_square_sum(empirical_modes)
+    etalon = calculate_mean_square_sum(etalon_data.get_etalon_emd())
+    current = calculate_mean_square_sum(empirical_modes)
     mode_count = min(etalon.shape[0], current.shape[0])
     return (current[:mode_count] - etalon[:mode_count]) / etalon[:mode_count]
 
-
 def deviation_interpretaion(deviation: np.array):
-    if max(abs(deviation)) > 50:
+    if max(abs(deviation)) > 40:
         return "значительные виляния"
     return "не выявлено значительных виляний"
