95c146c9e7befa55fe25814be6b3189aec05b0a9
c4llery <anton.rusanov.04@gmail.com>
2024-10-17 19:52:46 +0300

    update request for getting info with timestamps

--- backend/main.py
+++ backend/main.py
@@ -4,30 +4,28 @@ from datetime import date
 import algo
 from pydantic import BaseModel
 
-class Data(BaseModel):
-    start: date
-    finish: date
-    result: str
+class TimeStampData(BaseModel): # модель для обработки запроса по предоставлению данных по таймстемпам
+    start: str # начальный таймстемп
+    finish: str # конечный таймстемп
 
 
-
+app = FastAPI() 
 data = pd.read_csv("data.csv")
+numbers = [i for i in range(data.shape[0])]
 data["time"] = pd.to_datetime(data["ts"], format='mixed')
 data.index = data['ts']
-app = FastAPI()
-
 data = data.fillna('')
-@app.get("/data/{timestamps}")
-def find_timestamp(timestamps: str):
-    times = pd.Series(timestamps.split(','))
+
+@app.post("/data_timestamps/")
+async def find_timestamp(pass_data: TimeStampData):
+    times = pd.Series((pass_data.start, pass_data.finish))
     times = pd.DataFrame(times, columns=["time"])
     times['time'] = pd.to_datetime(times["time"], format='mixed')
     start = times.at[0, "time"]
     finish = times.at[1, "time"]
-    piece_of_data = data[(finish >= data["time"]) & (data['time'] >= start)]
-    return piece_of_data.to_csv()
-
-@app.post("/")
-def getData(data: Data):
-    data.result = algo.work()
-    return data
+    condition = (data['time'] >= start) & (data['time'] <= finish)
+    indices = data.index[condition].tolist()
+    piece_of_data = list()
+    for i in indices:
+        piece_of_data.append(data.loc[i])
+    return piece_of_data    
