afff0c317184d52922cac1a0b176f85db9439cc6
VariPor <maiorova.ev@phystech.edu>
2024-11-02 22:47:12 +0300

    alg: add speed into algorithm

--- core/processing_algorithm.py
+++ core/processing_algorithm.py
@@ -5,4 +5,6 @@ from . import alg_modes
 def calculate(df: pd.DataFrame):
     cte = alg_modes.calculate_value(df, 'cte')
     yaw_rate = alg_modes.calculate_value(df, 'yaw_rate')
-    return 'cte: ' + cte + ' yaw_rate: ' +  yaw_rate
+    speed = alg_modes.calculate_value(df, 'speed')
+    
+    return 'cte: ' + cte + ' yaw_rate: ' +  yaw_rate + ' speed: ' + speed
