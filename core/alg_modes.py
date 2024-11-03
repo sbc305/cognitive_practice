715f91939552f23ac344b4ede0fb9e0df0683de9
VariPor <maiorova.ev@phystech.edu>
2024-11-03 11:55:14 +0300

    alg: rewrite algorithm to class

--- core/alg_modes.py
+++ core/alg_modes.py
@@ -2,8 +2,6 @@ import pandas as pd
 import emd
 import numpy as np
 
-from . import etalon_data
-
 
 def EMD(column: pd.Series):
     imf = emd.sift.sift(column.to_numpy())
@@ -12,13 +10,6 @@ def EMD(column: pd.Series):
 
 def calculate_mean_square_sum(x: np.array):
     return sum([i * i for i in x]) / len(x)
-
-
-def compare_modes(empirical_modes: pd.Series, column):
-    etalon = calculate_mean_square_sum(etalon_data.get_etalon_emd(column))
-    current = calculate_mean_square_sum(empirical_modes)
-    mode_count = min(etalon.shape[0], current.shape[0])
-    return (current[:mode_count] - etalon[:mode_count]) / etalon[:mode_count]
 
 
 def count_extremes(x):
@@ -33,10 +24,3 @@ def deviation_interpretaion(deviation: np.array):
     if max(abs(deviation)) > 40:
         return "значительные виляния"
     return "не выявлено значительных виляний" 
-
-  
-def calculate_value(df: pd.DataFrame, column):
-    empirical_modes = EMD(df[column])
-    deviation = compare_modes(empirical_modes, column)
-    result = deviation_interpretaion(deviation)
-    return result
--- core/processing_algorithm.py
+++ core/processing_algorithm.py
@@ -1,10 +1,31 @@
 import pandas as pd
+import numpy as np
 from . import alg_modes
+from . import etalon_data
 
 
-def calculate(df: pd.DataFrame):
-    cte = alg_modes.calculate_value(df, 'cte')
-    yaw_rate = alg_modes.calculate_value(df, 'yaw_rate')
-    speed = alg_modes.calculate_value(df, 'speed')
+class ProcessingAlgorithm():
+    def __init__(self, data: pd.DataFrame, columns=['cte', 'yaw_rate', 'speed']):
+        self.data = data
+        self.columns = columns
+        self.set_etalon_data()
     
-    return 'cte: ' + cte + ' yaw_rate: ' +  yaw_rate + ' speed: ' + speed
+    
+    def set_etalon_data(self) -> None:
+        etalon_data.get_etalon_emd(self.columns)
+        etalon_data.get_etalon_extreme_count(self.columns)
+        
+        
+    def compare_modes(self, empirical_modes: pd.Series, column: str) -> np.array:
+        data = pd.read_csv("data/etalon" + column + "EMD.csv")
+        etalon = alg_modes.calculate_mean_square_sum(data)
+        current = alg_modes.calculate_mean_square_sum(empirical_modes)
+        mode_count = min(etalon.shape[0], current.shape[0])
+        return (current[:mode_count] - etalon[:mode_count]) / etalon[:mode_count]
+    
+    
+    def calculate(self, column: str) -> str:
+        empirical_modes = alg_modes.EMD(self.data[column])
+        deviation = self.compare_modes(empirical_modes, column)
+        result = alg_modes.deviation_interpretaion(deviation)
+        return result
