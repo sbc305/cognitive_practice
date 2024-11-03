4a58f818317b730fa90df7258b08405eb6cc636c
VariPor <maiorova.ev@phystech.edu>
2024-11-03 19:10:48 +0300

    alg: replace argument limit

--- core/processing_algorithm.py
+++ core/processing_algorithm.py
@@ -28,9 +28,9 @@ class ProcessingAlgorithm():
         return (current[:mode_count] - etalon[:mode_count]) / etalon[:mode_count]
     
     
-    def calculate(self, column: str) -> str:
+    def calculate(self, column: str, limit: float=40) -> str:
         deviation = self.compare_mode_with_etalon(column)
-        result = alg_modes.deviation_interpretaion(deviation)
+        result = alg_modes.deviation_interpretaion(deviation, limit)
         return result
     
     def get_EMD(self, column: str, etalon=False):
