cf5048d81b1ce4d3bf66fef90f96ca6cf531f95a
Artemiy2908 <adburenkov@gmail.com>
2024-11-12 21:40:37 +0300

    try to use vue.js, not html with vue

---
+++ frontend/my_first_app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <GraphComponent />
+  </div>
+</template>
+
+<script>
+import GraphComponent from './components/GraphComponent.vue';
+
+export default {
+  components: {
+    GraphComponent
+  }
+};
+</script>
+
+<style>
+/* Ваши стили */
+</style>
