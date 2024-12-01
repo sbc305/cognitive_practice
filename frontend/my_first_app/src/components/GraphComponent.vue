b592aa9aa71c790873817e56a47d2a83c6a81827
Artemiy2908 <adburenkov@gmail.com>
2024-12-01 13:22:38 +0300

    add id and update buttons

--- frontend/my_first_app/src/components/GraphComponent.vue
+++ frontend/my_first_app/src/components/GraphComponent.vue
@@ -1,26 +1,40 @@
 <template>
   <div>
-    <h2>Выберите временной интервал для построения графика</h2>
-    
-    <div>
-      <label for="startTime">Начальное время (YYYY-MM-DD HH:mm:ss):</label>
-      <input type="text" v-model="startTime" placeholder="2024-08-21 09:26:38" />
-      <div v-if="startTimeError" style="color: red;">{{ startTimeError }}</div>
-    </div>
-    
-    <div>
-      <label for="finishTime">Конечное время (YYYY-MM-DD HH:mm:ss):</label>
-      <input type="text" v-model="finishTime" placeholder="2024-08-21 09:27:38" />
-      <div v-if="finishTimeError" style="color: red;">{{ finishTimeError }}</div>
+    <h2>Выберите режим ввода</h2>
+    <button @click="toggleInputMode">{{ isTimeInput ? 'Переключить на ввод ID трассы' : 'Переключить на ввод времени' }}</button>
+
+    <div v-if="isTimeInput">
+      <h2>Выберите временной интервал для построения графика</h2>
+      
+      <div>
+        <label for="startTime">Начальное время (YYYY-MM-DD HH:mm:ss):</label>
+        <input type="text" v-model="startTime" placeholder="2024-08-21 09:26:38" />
+        <div v-if="startTimeError" style="color: red;">{{ startTimeError }}</div>
+      </div>
+      
+      <div>
+        <label for="finishTime">Конечное время (YYYY-MM-DD HH:mm:ss):</label>
+        <input type="text" v-model="finishTime" placeholder="2024-08-21 09:27:38" />
+        <div v-if="finishTimeError" style="color: red;">{{ finishTimeError }}</div>
+      </div>
+
+      <button @click="plotGraphByTime">Построить график</button>
     </div>
 
-    <button @click="plotGraph">Построить график</button>
+    <div v-else>
+      <h2>Выберите номер трассы для построения графика</h2>
+      <input type="number" v-model="traceNumber" placeholder="Введите номер трассы" min="0" max="9" />
+      <button @click="plotGraphById">Построить график</button>
+    </div>
+
     <button @click="toggleView">{{ isGraphView ? 'Показать данные' : 'Показать график' }}</button>
 
-    <div v-if="isGraphView" id="myDiv" style="width: 100%; height: 400px;"></div>
+    <!-- Изначально отображаем график -->
+    <div id="myDiv" style="width: 100%; height: 400px;" v-if="isGraphView"></div>
 
+    <!-- Отображаем данные, если не график -->
     <div v-if="!isGraphView">
-      <h3>Данные по выбранному интервалу</h3>
+      <h3>Данные по выбранному интервалу или трассе</h3>
       <table border="1">
         <thead>
           <tr>
@@ -42,7 +56,7 @@
 </template>
 
 <script>
-//библиотеки
+// библиотеки
 import Plotly from 'plotly.js'; 
 
 export default {
@@ -53,7 +67,9 @@ export default {
       startTimeError: '',
       finishTimeError: '',
       errorMessage: '',
-      isGraphView: true,
+      isGraphView: true, // Изначально показываем график
+      isTimeInput: true, // Начинаем с режима ввода времени
+      traceNumber: null,
       dataPoints: []
     };
   },
@@ -76,7 +92,6 @@ export default {
       this.startTimeError = this.validateTimestamp(this.startTime);
       this.finishTimeError = this.validateTimestamp(this.finishTime);
 
-      // Проверка на то, что начальное время не больше конечного
       if (!this.startTimeError && !this.finishTimeError) {
         const startDate = new Date(this.startTime);
         const finishDate = new Date(this.finishTime);
@@ -86,15 +101,14 @@ export default {
         }
       }
     },
-    
-    plotGraph() {
-      const url = 'http://localhost:8000/data/'; 
-      this.errorMessage = ''; // Сбрасываем сообщение об ошибке
-      this.validateTimes(); // Выполняем валидацию
 
-      // Проверяем наличие ошибок
+    plotGraphByTime() {
+      const url = 'http://localhost:8000/data/';
+      this.errorMessage = '';
+      this.validateTimes();
+
       if (this.startTimeError || this.finishTimeError) {
-        return; // Прерываем выполнение функции, если есть ошибки
+        return;
       }
 
       const user = {
@@ -114,45 +128,94 @@ export default {
         return response.json();
       })
       .then(data => {
-        this.dataPoints = data; // Сохраняем данные для таблицы
-        this.renderGraph(data); // Рисуем график с полученными данными
+        this.dataPoints = data;
+        this.renderGraph(data);
+        
+        // Сбрасываем режим отображения на график после получения данных
+        this.isGraphView = true;
+       })
+       .catch(error => {
+         console.error('Ошибка при получении JSON:', error);
+         this.errorMessage = 'Произошла ошибка при загрузке данных.';
+       });
+    },
+
+    plotGraphById() {
+      const url = 'http://localhost:8000/data/';
+      
+      if (this.traceNumber < 0 || this.traceNumber > 9) {
+        this.errorMessage = 'Такой трассы нет. Введите номер от 0 до 9.';
+        this.dataPoints = [];
+        return;
+      }
+
+      const user = { "file_id": this.traceNumber };
+
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+        body: JSON.stringify(user)
       })
-      .catch(error => {
-        console.error('Ошибка при получении JSON:', error);
-        this.errorMessage = 'Произошла ошибка при загрузке данных.';
-      });
+      .then(response => {
+        if (!response.ok) throw new Error('Сетевая ошибка: ответ не был успешным');
+        return response.json();
+      })
+      .then(data => {
+        this.dataPoints = data;
+        this.renderGraph(data);
+        
+        // Сбрасываем режим отображения на график после получения данных
+        this.isGraphView = true;
+       })
+       .catch(error => {
+         console.error('Ошибка при получении JSON:', error);
+         this.errorMessage = 'Произошла ошибка при загрузке данных.';
+       });
     },
-    
+
     renderGraph(data) {
-      const x = data.map(point => point.x);
-      const y = data.map(point => point.y);
+       const x = data.map(point => point.x);
+       const y = data.map(point => point.y);
 
-      const trace = {
-        x: x,
-        y: y,
-        mode: 'lines+markers',
-        type: 'scatter',
-        name: `Данные с ${this.startTime} по ${this.finishTime}`
-      };
+       const trace = {
+         x: x,
+         y: y,
+         mode: 'lines+markers',
+         type: 'scatter',
+         name: `Данные`
+       };
 
-      const layout = {
-        title: `График с ${this.startTime} по ${this.finishTime}`,
-        xaxis: { title: 'X координаты' },
-        yaxis: { title: 'Y координаты' }
-      };
+       const layout = {
+         title: `График`,
+         xaxis: { title: 'X координаты' },
+         yaxis: { title: 'Y координаты' }
+       };
 
-      Plotly.newPlot('myDiv', [trace], layout);
-    },
-    
-    toggleView() {
-      this.isGraphView = !this.isGraphView; // Переключаем режим отображения
-    }
-  }
+       Plotly.newPlot('myDiv', [trace], layout);
+     },
+
+     toggleView() {
+       this.isGraphView = !this.isGraphView; // Переключаем режим отображения
+     },
+
+     toggleInputMode() {
+       this.isTimeInput = !this.isTimeInput; // Переключаем режим ввода
+       // Сбрасываем ошибки и данные при переключении
+       this.startTime = '';
+       this.finishTime = '';
+       this.startTimeError = '';
+       this.finishTimeError = '';
+       this.traceNumber = null;
+       this.dataPoints = [];
+       this.errorMessage = '';
+     }
+   }
 };
 </script>
 
 <style scoped>
-
 input {
   width: 200px;
 }
