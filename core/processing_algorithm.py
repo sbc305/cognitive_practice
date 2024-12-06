f3914d0025b4bb7af965578986f445ebb37594ac
VariPor <maiorova.ev@phystech.edu>
2024-12-06 10:22:05 +0300

    alg: correction in energy calculation

--- core/processing_algorithm.py
+++ core/processing_algorithm.py
@@ -21,14 +21,14 @@ class ProcessingAlgorithm():
         
     def compare_mode_with_etalon(self, column: str) -> np.array: 
         # averaging correlation maxima across all modes
-        energy_etalon = np.sum(np.abs(self.etalon_modes[column]), axis=0)
-        energy_current = np.sum(np.abs(self.current_modes[column]), axis=0)
+        energy_etalon = np.sum(self.etalon_modes[column] ** 2, axis=0)
+        energy_current = np.sum(self.current_modes[column] ** 2, axis=0)
         correlation = correlate(self.etalon_modes[column] / energy_etalon, 
                                 self.current_modes[column] / energy_current)
         return np.mean(np.max(np.abs(correlation), axis=0))
     
     
-    def calculate(self, limit: float=0.004) -> str:
+    def calculate(self, limit: float=2) -> str:
         similarity = 0
         for column in self.columns:
             similarity += self.compare_mode_with_etalon(column)
